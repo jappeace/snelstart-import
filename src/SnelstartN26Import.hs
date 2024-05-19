718461ff0224889de4258e667d28529bb81648d2
Jappie Klooster <jappieklooster@hotmail.com>
2024-05-19 23:07:37 +0200

    draft out reading/writign

--- src/SnelstartN26Import.hs
+++ src/SnelstartN26Import.hs
@@ -3,5 +3,37 @@ module SnelstartN26Import
   )
 where
 
+import Data.Text
+
+data MutatieSoort = Overschijving | Diversen | Incassoe
+
+toCode :: MutatieSoort
+toCode = \case
+  Overschijving -> "OV"
+  Diversen -> "DV"
+  Incasso -> "IC"
+
+data BijAf = Bij | Af
+
+data Snelstart = Snelstart {
+  datum :: UTCTime,
+  naamBescrhijving :: Text,
+  rekening :: Text,
+  tegenRekening :: Text,
+  mutatieSoort ::  MutatieSoort, -- eg ook voor code
+  bijAf :: BijAf,
+  bedragEur :: Rational,
+  mededeling :: Text
+  }
+
+data TransactionType = MastercardPayment | OutgoingTransfer | Income | N26Referal | DirectDebit
+
+data N26 = N26  {
+  date :: UTCTime,
+  payee :: Text,
+  accountNumber :: Text
+  }
+
+
 main :: IO ()
 main = putStrLn "hello, world flaky"
