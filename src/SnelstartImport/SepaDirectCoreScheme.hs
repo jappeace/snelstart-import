7bfeb65b8d22c93a39d0788235d297e283ba6f20
Jappie Klooster <jappieklooster@hotmail.com>
2025-03-15 18:50:32 +0100

    I think this finniishes most of the xml parsing

--- src/SnelstartImport/SepaDirectCoreScheme.hs
+++ src/SnelstartImport/SepaDirectCoreScheme.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE RecordWildCards #-}
 
 -- | https://www.europeanpaymentscouncil.eu/sites/default/files/kb/file/2022-06/EPC130-08%20SDD%20Core%20C2PSP%20IG%202023%20V1.0.pdf
 --   this is some xml format the accountents asked support for
@@ -17,14 +18,19 @@ import Data.Text.Encoding (decodeUtf8)
 import qualified Data.Text as Text
 import Data.List
 import Data.Bifunctor(first)
+import Text.Read(readMaybe)
+import Data.Time(Day, parseTimeM, defaultTimeLocale)
 
 data SepaDirectCoreScheme = SepaDirectCoreScheme {
   -- -- | Unambiguous identification of the account of the
   -- -- creditor to which a credit entry will be posted as a
   -- -- result of the payment transaction.
   -- cdtrAcct :: Text,
-
-  dbtrAcct :: Text
+  endToEndId :: Text,
+  dbtrAcct :: Text,
+  dbtr :: Text,
+  instdAmt :: Currency,
+  dtOfSgntr :: Day
   } deriving Show
 
 tshow :: Show a => a -> Text
@@ -48,6 +54,8 @@ readSepaDirectCoreScheme contents = do
 --
 
 data SepaIssues = ExpectedOne [Node] Text
+                | ExpectedNumber Text Text
+                | ExpectedDate Text
                 deriving Show
 
 
@@ -60,9 +68,23 @@ assertOne label nodes =
 inner_ :: Node -> Text
 inner_ = decodeUtf8 . inner
 
+parseCurrency :: Text -> Text -> Either SepaIssues Currency
+parseCurrency label txt = case readMaybe (Text.unpack txt) of
+  Just number -> Right $ Currency number
+  Nothing -> Left (ExpectedNumber label txt)
+
+parseDay :: Text -> Text -> Either SepaIssues Day
+parseDay label txt = case parseTimeM True defaultTimeLocale "%F" (Text.unpack txt) of
+  Nothing -> Left $ ExpectedDate label
+  Just day -> Right day
+
 parseSepa :: Node -> Either SepaIssues SepaDirectCoreScheme
 parseSepa node = do
-  dbtrNode <- assertOne "dbtracct" $ dig "IBAN" =<< dig "Id" =<< dig "DbtrAcct" node
+  dbtr <- inner_ <$> assertOne "dbtr" (dig "nm" =<< dig "dbtr" node)
+  dbtrAcct <- inner_ <$> assertOne "dbtracct" (dig "IBAN" =<< dig "Id" =<< dig "DbtrAcct" node)
+  endToEndId <- inner_ <$> assertOne "endToEndId" (dig "EndToEndId" =<< dig "PmtId" node)
+  instdAmt <- parseCurrency "instdAmt" . inner_ =<< assertOne "instdAmt" (dig "instdAmt" node)
+  dtOfSgntr <- parseDay "dtOfSgntr" . inner_ =<< assertOne "dtOfSgntr" (dig "DtOfSgntr" =<< dig "MndtRltdInf" =<< dig "DrctDbtTx" node)
   Right $ SepaDirectCoreScheme {
-      dbtrAcct = inner_ dbtrNode
+    ..
     }
